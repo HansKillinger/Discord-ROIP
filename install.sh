8f203dc8cfd1fde9162abaa3ce51652fe85dad81
Hans Killinger <hanskillinger@protonmail.com>
2023-10-08 20:01:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo apt install snapd python3-pip sudo apt-get install libportaudio2 libasound2
 python3 -m pip install sounddevice numpy pyserial pynput
 sudo snap install discord
 sudo adduser $USER dialout
+cp Discord-ROIP.desktop ~/.config/autostart/Discord-ROIP.desktop
 echo "Install Complete"
 echo "Listing Audio Sources"
 pactl list sources | grep -e "Name"
